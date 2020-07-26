a571190563a82a468905424cd3954ab5be76b53f
linuxssd <david.coronel@outlook.com>
2020-07-26 18:50:22 -0500

    Removed RxJava fun transformations

--- data/src/main/java/com/droid/clean/data/datasources/impl/LocalForecastDataSourceImpl.kt
+++ data/src/main/java/com/droid/clean/data/datasources/impl/LocalForecastDataSourceImpl.kt
@@ -3,24 +3,15 @@ package com.droid.clean.data.datasources.impl
 import com.droid.clean.data.repository.LocalForecastDataSource
 import com.droid.clean.domain.models.Weather
 import io.reactivex.Observable
-import io.reactivex.functions.BiFunction
-import java.util.concurrent.TimeUnit
-import kotlin.random.Random
 
 class LocalForecastDataSourceImpl :
     LocalForecastDataSource {
     override fun getWeather(): Observable<Weather> {
         val oneToFive = 1..5
-        return Observable.zip(
-            Observable.fromIterable(oneToFive),
-            Observable.interval(2, TimeUnit.SECONDS),
-            BiFunction<Int, Long, Long> { a, b -> return@BiFunction a.toLong() }
-        )
-            .doOnNext { println("emitido " + it) }
-            .map { it + 100 }
-            .flatMap {
-                println("segungo flatmap" + it)
-                return@flatMap Observable.just(Weather(temperature = (5..30).random()))
+        return Observable.fromIterable(oneToFive)
+            .map {
+                Thread.sleep(2000)
+                Weather(temperature = (5..30).random())
             }
     }
 }
